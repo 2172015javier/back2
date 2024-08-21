e656b757811dace3723931489455da48ca9f655e
javier <narcisista12@gmail.com>
2024-08-21 13:57:55 -0500

    archivo pk

--- models/user.js
+++ models/user.js
@@ -1,67 +1,40 @@
-
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const UsuarioSchema = Schema({
-    username: {
-        type: String,
-        required: [true, 'EL nombre es obligatorio']
-    },
-  
-    password: {
-        type: String,
-        required: [true, 'La contraseña es obligatoria'],
-    },
-    role: {
-        type: String,
-        required: true,
-        // Administrador y Analista
-        enum: ['Administrador', 'Analista'] 
-    },
-    permisos:{
-        type: [String], 
-        enum: ['lectura', 'escritura'],
-        required: true 
-    },
-    estado: {
-        type: Boolean   ,
-        default: true
-    },
-    google: {
-        type: Boolean,
-        default:false
-    },
-    
-    
+  username: {
+    type: String,
+    required: [true, "EL nombre es obligatorio"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "La contraseña es obligatoria"],
+  },
+  role: {
+    type: String,
+    required: true,
+    // Administrador y Analista
+    enum: ["Administrador", "Analista"],
+  },
+  permisos: {
+    type: [String],
+    enum: ["lectura", "escritura"],
+    required: true,
+  },
+  estado: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-UsuarioSchema.methods.toJSON = function(){
-    const { __v, password, _id, ...usuario } = this.toObject();
-    usuario.uid = _id; 
-    return usuario
-}
+UsuarioSchema.methods.toJSON = function () {
+  const { __v, password, _id, ...usuario } = this.toObject();
+  usuario.uid = _id;
+  return usuario;
+};
 
-module.exports = model('User', UsuarioSchema);
-
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs');
-
-// const userSchema = new mongoose.Schema({
-//   username: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   role: { type: String, enum: ['Administrador', 'Analista'], required: true },
-//   permissions: { type: [String], enum: ['lectura', 'escritura'], required: true },
-//   active: { type: Boolean, default: true }
-// });
-
-// userSchema.pre('save', async function(next) {
-//   if (!this.isModified('password')) return next();
-//   this.password = await bcrypt.hash(this.password, 10);
-//   next();
-// });
-
-// userSchema.methods.matchPassword = async function(enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
-
-// const User = mongoose.model('User', userSchema);
-// module.exports = User;
+module.exports = model("User", UsuarioSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const { createUser, getUsers, deactivateUser, changeUserRole } = require('../controllers/userController');
-const{check} = require('express-validator');
+const express = require("express");
+const {
+  createUser,
+  getUsers,
+  deactivateUser,
+  changeUserRole,
+} = require("../controllers/userController");
+const { check } = require("express-validator");
 const router = express.Router();
 
-router.post('/', createUser);
-router.get('/', getUsers);
-router.delete('/:id',[
+router.post("/", createUser);
+router.get("/", getUsers);
+router.delete(
+  "/:id",
+  [
     // check('id', 'No es un ID valido').isMongoId()
-], deactivateUser);
-router.patch('/:id/:role1', changeUserRole);
+  ],
+  deactivateUser
+);
+router.patch("/:id/:role1", changeUserRole);
 
 module.exports = router;
